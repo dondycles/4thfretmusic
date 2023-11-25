1ce5916e307477fdc23abb843e752b4bc9f50ec1
dondycles <johnrod.dondoyano@gmail.com>
2023-11-26 00:46:58 +0800

    modified:   app/lyrics/[title]/page.tsx 	modified:   components/Nav.tsx

--- app/lyrics/[title]/page.tsx
+++ app/lyrics/[title]/page.tsx
@@ -11,12 +11,14 @@ export default function Lyrics({ params }: { params: { title: string } }) {
   }
   return (
     <div className="min-h-fit w-full pt-4 px-4 sm:px-16 md:px-32 lg:px-64 z-10">
-      <article className="flex flex-col gap-4 text-sm justify-center items-center">
+      <article className="flex flex-col gap-4 text-sm w-fit justify-center mx-auto">
         <header className="font-agbalumo text-3xl text-primary ">
           <h1> {findSongByTitle(params.title)?.title}</h1>
-          <h2 className="text-sm text-foreground pb-2">by 4th Fret</h2>
+          <h2 className="text-sm sm:text-base text-foreground pb-2">
+            by 4th Fret
+          </h2>
         </header>
-        <div className="flex gap-4 flex-col">
+        <div className="flex gap-4 flex-col ">
           {findSongByTitle(params.title)?.lyrics?.map((p, i) => {
             return (
               <div className="" key={i}>
